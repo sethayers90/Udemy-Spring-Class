a06c6f85a414692f14f2beec8e93f233487ac982
SETH R AYERS <sethayers90@gmail.com>
2020-07-27 20:21:48 -0400

    Add files via upload

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -1,11 +1,15 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	private String country;
-	
+	private String favoriteLanguage;
+	private LinkedHashMap<String, String> countryOptions;
+	private String[] operatingSystems;
 	public String getCountry() {
 		return country;
 	}
@@ -15,7 +19,13 @@ public class Student {
 	}
 
 	public Student() {
-		
+		//populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("US", "United States");
+
 	}
 
 	public String getFirstName() {
@@ -33,5 +43,27 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+
+
 	
 }
